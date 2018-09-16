df477fd06238de6c59785e22019cc353914aad64
kunzp <kunz.pas@gmail.com>
2018-09-16 20:21:03 +0200

    created project

---
+++ two_way_authentification/two_way_authentification/two_way_authentification/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace two_way_authentification
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ two_way_authentification/two_way_authentification/two_way_authentification/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace two_way_authentification.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login()
+        {
+            var username = Request["username"];
+            var password = Request["passowrd"];
+
+            if (username == "test" && password == "test")
+            {
+                var request = (HttpWebRequest) WebRequest.Create("https://test.nexmo.com/sms/json");
+                var secret = "TEST_SECRET";
+
+                var postData = "api_key=182770bd";
+                postData += "&api_secret=HOMk5odxVnOMD3sk";
+                postData += "&to=41763055505 ";
+                postData += "&from=\"NEXMO\"";
+                postData += "&text=\""+ secret +"\"";
+
+                var data = Encoding.ASCII.GetBytes(postData);
+
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = data.Length;
+
+                using (var stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+
+                var response = (HttpWebResponse) request.GetResponse();
+
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+                ViewBag.Message = responseString;
+            }
+            else
+            {
+                ViewBag.Message = "Wrong Credentials";
+            }
+            return View();
+        }
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
