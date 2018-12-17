81cee01f3393c647ea1f6e0008f7c9387aa7345c
Paeschu <kunz.pas@gmail.com>
2018-12-17 09:04:01 +0100

    added role based authorization

---
+++ role_based_authorization/role_based_authorization/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace role_based_authorization
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ role_based_authorization/role_based_authorization/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace role_based_authorization.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login()
+        {
+            var username = Request["username"];
+            var password = Request["password"];
+
+            if(username == "admin" & password == "test")
+            {
+                Session["username"] = "admin";
+                return RedirectToAction("Dashboard", "Admin");
+            }
+            else if (username == "user" & password == "test")
+            {
+                Session["username"] = "user";
+                return RedirectToAction("Dashboard", "User");
+            }
+            else
+            {
+                ViewBag.Message = "Wrong Credentials";
+            }
+            return View();
+        }
+    }
+}
